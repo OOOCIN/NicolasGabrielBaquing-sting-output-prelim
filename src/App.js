81dad24a03e822e82a8256ffc00a959b9162e8ca
OOOCIN <nicolas_baquing@rcc.edu.ph>
2024-09-28 10:48:48 +0800

    number 1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StringOutput = () => {
+  const message = "Hello Everyone! :)";
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
+};
+
+export default StringOutput;
